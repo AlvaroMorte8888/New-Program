10cc1a061b48a8e4daa8bfa3fc30b75fe4c309c2
AlvaroMorte8888 <alvaromorte8888@gmail.com>
2020-05-16 17:44:56 +0300

    Show latest posts

--- app.rb
+++ app.rb
@@ -32,8 +32,9 @@ end
 get '/' do
 	# выбираем список постов из БД
 	
-	results = db.execute 'select * from Posts'
-		erb :index			
+	@results = @db.execute 'select * from Posts order by id desc'
+	
+	erb :index			
 end
 
 #обработчик get-запроса new
