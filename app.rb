da7ae2bcd56f1448e471f0891d0891ab7238a76f
AlvaroMorte8888 <alvaromorte8888@gmail.com>
2020-05-16 17:27:09 +0300

    Save content to datebase

--- app.rb
+++ app.rb
@@ -49,5 +49,8 @@ post '/new' do
 		@error = 'Type post text'
 		return erb :new 
 	end	
-  erb "You typed:  #{content}"
+  
+	@db.execute 'insert into Posts (content,created_date) values (?,datetime())', [content]
+
+	erb "You typed:  #{content}"
 end
